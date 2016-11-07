c1ada193b03df1fd36ffbf942f03a5dc753ddad7
Joel Baticz <joel.baticz@gmail.com>
2016-11-07 13:27:28 +1100

    Added Typeahead service to Index page & Additional stylings

---
+++ src/GrandeTravel/Controllers/ApiController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using GrandeTravel.Models;
+using GrandeTravel.ViewModels;
+using GrandeTravel.Services;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace GrandeTravel.Controllers
+{
+    public class ApiController : Controller
+    {
+        private IPackageRepository _packageRepo;
+
+        public ApiController(IPackageRepository packageRepo)
+        {
+            _packageRepo = packageRepo;
+        }
+
+
+        public JsonResult GetAllPackageLocation()
+        {
+            /*
+            LocationData ld = new LocationData
+            {
+                Locations = _packageRepo.GetAll().Select(p => p.Location).Distinct()
+            };
+
+            
+            return Json(ld);
+            */
+            /*return Json(_packageRepo.GetAll().Select(p => p.Location).Distinct());*/
+
+            return Json(_packageRepo.Query(p => p.IsActive == true).Select(p => p.Location).Distinct());
+        }
+    }
+}
